5d222aadde6e73ee781dc9503d9c688c4dfece3a
pragith98 <lakshanxp@hotmail.com>
2023-02-08 14:18:56 +0530

    import home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Header from "./components/Header";
 import React from "react";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
+      <Home/>
     </React.Fragment>
   );
 }
