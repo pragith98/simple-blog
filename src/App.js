81410313656bf03fcfacf9b23e6ae058b827fea2
pragith98 <lakshanxp@hotmail.com>
2023-02-08 14:29:07 +0530

    import and create routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
@@ -6,9 +8,13 @@ import CreatePost from "./pages/CreatePost";
 function App() {
   return (
     <React.Fragment>
-      <Header/>
-      <Home/>
-      <CreatePost/>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="createPost" element={<CreatePost />} />
+        </Routes>
+      </BrowserRouter>
     </React.Fragment>
   );
 }
