5e3083cf63d952ee1be40eea6de3b2433d18163d
pragith98 <lakshanxp@hotmail.com>
2023-02-08 15:34:21 +0530

    create function to displayPostCards

--- src/components/Post/PostCardList.jsx
+++ src/components/Post/PostCardList.jsx
@@ -1,11 +1,29 @@
 import PostCard from "./PostCard";
+import { useState } from "react";
 
 function PostCardList() {
-  return (
-    <div className="row">
-      <PostCard />
-    </div>
-  );
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "test post 1",
+      content:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit autem eaque omnis laboriosam sit minus dolore consectetur obcaecati, nobis tenetur accusamus eveniet ipsum maxime temporibus itaque sapiente ducimus consequatur aspernatur.",
+    },
+    {
+      id: 2,
+      title: "test post 2",
+      content:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit autem eaque omnis laboriosam sit minus dolore consectetur obcaecati, nobis tenetur accusamus eveniet ipsum maxime temporibus itaque sapiente ducimus consequatur aspernatur.",
+    },
+  ]);
+
+  const displayPostCards = () => {
+    return posts.map((post) => {
+      return <PostCard post={post} key={post.id}/>;
+    });
+  };
+
+  return <div className="row">{displayPostCards()}</div>;
 }
 
 export default PostCardList;
