114876f3fb66c0cc232db74d0067c2cfc4278104
pragith98 <lakshanxp@hotmail.com>
2023-02-08 15:34:35 +0530

    set props

--- src/components/Post/PostCard.jsx
+++ src/components/Post/PostCard.jsx
@@ -1,13 +1,10 @@
-function PostCard() {
+function PostCard(props) {
   return (
     <div className="col-sm-3 mt-4">
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">Special title treatment</h5>
-          <p className="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
-          </p>
+          <h5 className="card-title">{props.post.title}</h5>
+          <p className="card-text">{props.post.content}</p>
           <a href="#" className="btn btn-primary me-1">
             View
           </a>
