d4d1495d7c91d3373f804de87c1eab4a08016ee4
pragith98 <lakshanxp@hotmail.com>
2023-02-08 14:23:06 +0530

    import createPost page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home";
+import Post from "./pages/Post";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
       <Home/>
+      <Post/>
+      <CreatePost/>
     </React.Fragment>
   );
 }
