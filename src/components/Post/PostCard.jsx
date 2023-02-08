c9de5ebf362375ad4076a4df4841e2359a507d85
pragith98 <lakshanxp@hotmail.com>
2023-02-08 15:40:23 +0530

    short the long titles and contents

--- src/components/Post/PostCard.jsx
+++ src/components/Post/PostCard.jsx
@@ -3,8 +3,8 @@ function PostCard(props) {
     <div className="col-sm-3 mt-4">
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">{props.post.title}</h5>
-          <p className="card-text">{props.post.content}</p>
+          <h5 className="card-title">{(props.post.title.length >20) ? (props.post.title.slice(0,20)+" ...") :props.post.title}</h5>
+          <p className="card-text">{(props.post.content.length >80) ? (props.post.content.slice(0,80)+" ...") : props.post.content}</p>
           <a href="#" className="btn btn-primary me-1">
             View
           </a>
